7fc0cc9a17672605696fb10e3fb3a5aee801e029
Mauricio <villa669@umn.edu>
2025-12-12 20:53:06 -0600

    relation

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ const id = nanoid();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/paragraph'>
+    <BrowserRouter basename='/relation'>
       <Chat id={id} />
     </BrowserRouter>
   </React.StrictMode>
